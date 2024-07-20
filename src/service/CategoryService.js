b97262f1459faa0079a7df4404a027abf61557d4
makechi <makechieric9@gmail.com>
2024-07-20 13:04:08 +0300

    Update category service

--- src/service/CategoryService.js
+++ src/service/CategoryService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const CATEGORIES_API_BASE_URL = "http://localhost:3000/api/categories";
+const CATEGORIES_API_BASE_URL = "/api/categories";
 
 const CategoryService = {
     getAllCategories: () => {
