e4002e49148a0509832c5b5e2f2b3b8f41e0f48f
makechi <makechieric9@gmail.com>
2024-07-20 12:29:30 +0300

    Update categories api

--- src/app/api/categories/[id]/route.js
+++ src/app/api/categories/[id]/route.js
@@ -1,35 +1,48 @@
-import {connectToDB} from "@/utils/database";
+import { connectToDB } from "@/utils/database";
 import Category from "@/models/category";
 
-export const GET = async (request, {params}) => {
+const corsHeaders = {
+    'Access-Control-Allow-Origin': 'https://inventory-management-system-7mrzg4o6i-makechi02s-projects.vercel.app',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+};
+
+export const GET = async (request, { params }) => {
     const categoryId = params.id;
 
     try {
         await connectToDB();
-        const category = await Category.find({_id: categoryId});
-        return new Response(JSON.stringify(category));
+        const category = await Category.find({ _id: categoryId });
+        return new Response(JSON.stringify(category), { headers: corsHeaders });
     } catch (e) {
-        return new Response("Category with id not found", {status: 500});
+        return new Response("Category with id not found", { status: 500, headers: corsHeaders });
     }
 }
 
-export const PUT = async (request, {params}) => {
+export const PUT = async (request, { params }) => {
     const category = await request.json();
     try {
         await connectToDB();
-        const response = await Category.updateOne({_id: params.id}, category);
-        return new Response(JSON.stringify(response), {status: 200});
+        const response = await Category.updateOne({ _id: params.id }, category);
+        return new Response(JSON.stringify(response), { status: 200, headers: corsHeaders });
     } catch (e) {
-        return new Response("Failed to update category", {status: 500});
+        return new Response("Failed to update category", { status: 500, headers: corsHeaders });
     }
 }
 
-export const DELETE = async (request, {params}) => {
+export const DELETE = async (request, { params }) => {
     try {
         await connectToDB();
-        await Category.deleteOne({_id: params.id});
-        return new Response("Success", {status: 200});
+        await Category.deleteOne({ _id: params.id });
+        return new Response("Success", { status: 200, headers: corsHeaders });
     } catch (e) {
-        return new Response("Failed to delete category");
+        return new Response("Failed to delete category", { status: 500, headers: corsHeaders });
     }
+}
+
+export const OPTIONS = async () => {
+    return new Response(null, {
+        status: 200,
+        headers: corsHeaders,
+    });
 }
